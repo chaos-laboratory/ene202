b6bba529fa9d14f01ef0649d3dc7c370883dd601
plazovskis <peterlazovskis@gmail.com>
2025-09-16 13:29:23 -0400

    Uploading Lab2 extended code

---
+++ labs/lab_2/apps_script_lab2.js
@@ -0,0 +1,21 @@
+function collectData() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  try {
+    var device_id = "YOUR_DEVICE_ID";
+    var token = "CLASS_TOKEN_HERE";
+    var response = UrlFetchApp.fetch(
+      `https://api.particle.io/v1/devices/${device_id}/temp?`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    var payload = JSON.parse(response.getContentText()); // parse the JSON text
+    var timestamp = new Date();
+    var data = payload.result;
+    sheet.appendRow([timestamp, data]); // append the date, data1, data2 to the sheet
+  } catch (e) {
+    Logger.log("Could not connect to Particle.io. Check your device_id and token.");
+  }
+}
