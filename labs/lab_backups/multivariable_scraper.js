55d1355d90df797d0cc443e9a94c7a82d1123879
James <32513142+jamesryancoleman@users.noreply.github.com>
2025-09-23 10:29:20 -0400

    updated comments and renamed folder

--- labs/lab_backups/multivariable_scraper.js
+++ labs/lab_backups/multivariable_scraper.js
@@ -2,26 +2,17 @@
 // make sure your device_id and token are correct!
 var device_id = "YOUR_DEVICE_ID";
 var token = "A_VALID_TOKEN";
-/*
-  enter all your data labels from your firmware here. E.g., 
-  if you have the line:
 
-    Particle.variable("therm", therm);
-    
-  you label is "term".
-*/
+// Enter all your data labels from your firmware in all_labels.
+// E.g., if you have the lines:
+//
+//   Particle.variable("therm", thermistor);
+//   Particle.variable("humid", humidity);
+//
+// all_labels should be ["term", "humid"].
 var all_labels = ["LABEL_1", "LABEL_2", "LABEL_N"]; 
 
-/*
-  enter all your data labels from your firmware here. E.g., 
-  if you have the line:
-
-    Particle.variable("therm", therm);
-    
-  you label is "term".
-*/
-var all_labels = ["temp", "humid", "therm"]; // add or remove labels in quotes as needed
-
+// == NO CHANGES BELOW HERE ==
 // this function should be run on a timer-based trigger
 function collectData() {
   var sheet = SpreadsheetApp.getActiveSheet();
@@ -32,15 +23,16 @@ function collectData() {
       data.push(individual_value);
     } catch (e) {
       Logger.log(e);
-      data.push("error"); // saves a blank cell if there was an error
+      data.push("error"); // saves an error message cell if there was an error
     }
   }
 
-  // append the date, data1, data... to the sheet
+  // append the timestamp, data1, data... to the sheet
   var timestamp = new Date();
   sheet.appendRow([timestamp, ...data]); 
 }
 
+// encapsulates all fetching and parsing to retrieve the sensor values
 function getVariable(label) {
   var response = UrlFetchApp.fetch(
     `https://api.particle.io/v1/devices/${device_id}/${label}?`,
@@ -49,7 +41,7 @@ function getVariable(label) {
         Authorization: `Bearer ${token}`,
       },
     }
-  );   
+  );   ``
   var payload = JSON.parse(response.getContentText());
   var data = payload.result;
   return data;
