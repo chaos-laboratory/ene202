3d58d5ed60985a9976b7ad9f93e704d1bc68dbeb
plazovskis <peterlazovskis@gmail.com>
2025-09-29 18:15:04 -0400

    Update 04_led_therm_sub.ino

--- labs/firmware/04_led_therm_sub.ino
+++ labs/firmware/04_led_therm_sub.ino
@@ -1,73 +1,29 @@
-//libraries that must be included
+// Include Particle Device OS APIs
+#include "Particle.h"
 
-#include <math.h>
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(AUTOMATIC);
 
-/*
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-  This example code is in the public domain: https://docs.arduino.cc/built-in-examples/basics/Fade
-*/
+// Show system, cloud connectivity, and application logs over USB
+// View logs with CLI using 'particle serial monitor --follow'
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
-//global variable definition for the temperature
-double temperature = 0;
-// a variable to help see the received temperature data on particle console
-double data = 0.00;  
-// the PWM pin the LED is attached to. You'll have to add this.
-int led = D1;
+//---------------------------------------------------------------------------
 
+int led = A2;
 
-//setup
-void setup() {
-  
-  // declare pin to be an output:
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-  
-  // PL we are here adding an input signal, which we receive from a publisher, thanks to our subscribe function:
-  Particle.subscribe("temperature", toggleLED);
-  Particle.variable("temperature", data);
+void setup () {
+    pinMode(led, OUTPUT);
+    digitalWrite(led, LOW);
+    Particle.subscribe("KAT SAID", ledON);
 }
 
-
-void loop() {
-    
+void ledON(const char *event, const char *data) {
+    digitalWrite(led, HIGH);
+    delay(300);
+    digitalWrite (led, LOW);
+    delay(300);
+    digitalWrite(led, HIGH);
+    delay(300);
+    digitalWrite (led, LOW);
 }
-
-
-//Event handler function:
-void toggleLED(const char *event, const char *dataIn) { 
-    
-    data = double(*dataIn); // data = int(*dataIn);
-    // if (*dataIn >= 29) {
-    //     digitalWrite(led, HIGH);
-    // } 
-    // else digitalWrite(led, LOW);
-    
-    if (*dataIn) {
-        // Convert string to double
-        temperature = data;
-        // Control LED based on temperature
-        if (temperature >= 29.0) {
-            digitalWrite(led, HIGH);
-        } 
-        else digitalWrite(led, LOW);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
