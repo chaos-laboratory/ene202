aef1ea3ffbb358fa78f5374ee3df3e53fb576c99
plazovskis <peterlazovskis@gmail.com>
2025-09-29 23:28:07 -0400

    Added comments to Pub and Sub

--- labs/firmware/04_led_therm_pub.ino
+++ labs/firmware/04_led_therm_pub.ino
@@ -1,3 +1,6 @@
+//---------------------------------------------------------------------------
+// No need to read anything in this section:
+
 // Include Particle Device OS APIs
 #include "Particle.h"
 #include <math.h>
@@ -11,26 +14,35 @@ SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
 //---------------------------------------------------------------------------
 
+// Specify which pin your thermistor is connected to. Most likely, it will be A0, from previous labs.
+// As well, specify your sample rate. 3*1000 = 3 seconds, and is the quickest we recommend. 5 to 10 seconds is totally fine.
+// Finally, specify the temperature 'threshold' above which you would like the led on the 'subscribing' microcontroller to turn on.
 int thermPin = A0;
 int sample_rate = 3*1000;
 double temperature = 0;
 int threshold = 29;
 
+// These terms and numbers are used by the firmware in the next section to convert the incoming analog signal from the thermistor into a comprehensible temperature value.
+// No need to do anything here.
 #define TEMPERATURENOMINAL 25   
 #define NUMSAMPLES 5
 #define BCOEFFICIENT 3950
 #define SERIESRESISTOR 10000  
-
 uint16_t samples[NUMSAMPLES];
 uint8_t i;
 float average;
 
 //---------------------------------------------------------------------------
 
+// Here we tell Particle to make the thermistor temperature into a variable that we can read in the Particle Console.
 void setup() {
     Particle.variable("thermistor", temperature);
 }
 
+// Here we make a loop, that performs the following operation, at the sample rate we have specified above.
+// The operation is, first, to read the temperature from the thermistor pin. This temperature value is the result of the analog voltage conversion by the code snippet at the end of this file.
+// If the temperature value is above the threshold we specified above, we tell Particle to publish this value to the "KAT SAID" channel
+// Importanly, we tell it to publish this value as a string, which will make it readable by the 'subscribe' command on the other microcontroller.
 void loop() {
     temperature = therm(thermPin);
     if(temperature >= threshold) {
@@ -39,6 +51,9 @@ void loop() {
     delay (sample_rate);
 }
 
+//---------------------------------------------------------------------------
+// And down here is where the analog thermistor coltage signal gets converted into a temperature value.
+// No need to do anything here. Just marvel at the complexity and potential of some good codin'.
 
 double therm(int pin) {
   
--- labs/firmware/04_led_therm_sub.ino
+++ labs/firmware/04_led_therm_sub.ino
@@ -1,3 +1,6 @@
+//---------------------------------------------------------------------------
+// No need to read anything in this section:
+
 // Include Particle Device OS APIs
 #include "Particle.h"
 
@@ -10,14 +13,21 @@ SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
 //---------------------------------------------------------------------------
 
+// Choose a pin compatible with PWM on your microcontroller; this will now be called 'led'. Refer to Photon2 Pinout diagram:
 int led = A2;
 
+// Here, you're telling your 'led' pin, to output signals only, and to begin in the off position.
+// Next, you are subscribing to the 'channel' "KAT SAID" - or whatever arbitrary name that you sepcified in your 'publish' code
+// Please change 'KAT SAID' to whatever word / phrase you have chosen in your 'publish' code
 void setup () {
     pinMode(led, OUTPUT);
     digitalWrite(led, LOW);
     Particle.subscribe("KAT SAID", ledON);
 }
 
+// The first line translates incoming signals from your publishing device, into readable data. 
+// Then, the subsequent lines switch the led on for 300ms, and then off for 3ms, several times. Feel free to edit this as desired.
+// Once the 'subscribed' channel stops transmitting a signal, the led will automatically stop blinking.
 void ledON(const char *event, const char *data) {
     digitalWrite(led, HIGH);
     delay(300);
